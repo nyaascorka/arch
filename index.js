95fc2295f95e07f1116e31dea23da403f4e1cc31
nyaascorka <nyaascorka@gmail.com>
2024-12-23 23:40:18 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-
+/*
 app.set('views', './views');
 app.set('view engine', 'pug');
 
 app.get('/', (_, res) => {
   res.render('index', {test: 'testostring', id: 200});
 });
-
+*/
+app.get('/', (_, res) => {
+  res.send("text");
+});
 app.listen(port, () => {
   console.log(`Example app listening on ${port}`);
 });
